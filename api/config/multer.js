716dd93832d71ada8564fba8b9493003af4477fa
vinayakvenugopal <110168347+vinayakvenugopal@users.noreply.github.com>
2023-11-23 12:37:55 +0530

    multer modified

--- api/config/multer.js
+++ api/config/multer.js
@@ -1,41 +1,60 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
+
+const ensureDirectoryExists = (directory) => {
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory, { recursive: true });
+  }
+};
 
 const storage = multer.diskStorage({
-
-    destination: (req, file, cb) => { cb(null, "api/Public/HotelImages") },
-  
-    filename: (req, file, cb) => { cb( null, file.fieldname + "_" + Date.now() + path.extname(file.originalname) ) }
-    
-  
-  });
-  const storageForRoom = multer.diskStorage({
-
-    destination: (req, file, cb) => { cb(null, "api/Public/RoomImages") },
-  
-    filename: (req, file, cb) => { cb( null, file.fieldname + "_" + Date.now() + path.extname(file.originalname) ) }
-    
-  
-  });
-  const fileFilter = (req, file, cb) => {
-
-    if (file.mimetype.startsWith("image/")) {
-      
-      cb(null, true);
-  
-    } else {
-  
-      cb(new Error("Only images are allowed!"), false);
-  
+  destination: (req, file, cb) => {
+    const destinationPath = path.join(__dirname, "api", "Public", "HotelImages");
+    ensureDirectoryExists(destinationPath);
+    cb(null, destinationPath);
+  },
+  filename: (req, file, cb) => {
+    try {
+      const filename = file.fieldname + "_" + Date.now() + path.extname(file.originalname);
+      cb(null, filename);
+    } catch (err) {
+      cb(err);
     }
-  
-  };
-
-  // Initialize Multer with the storage configuration
-  export const multerUploadHotelImages = multer({ storage: storage,
-    fileFilter: fileFilter,
+  }
 });
 
-export const multerUploadRoomImages = multer({ storage: storageForRoom,
+const storageForRoom = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const destinationPath = path.join(__dirname, "api", "Public", "RoomImages");
+    ensureDirectoryExists(destinationPath);
+    cb(null, destinationPath);
+  },
+  filename: (req, file, cb) => {
+    try {
+      const filename = file.fieldname + "_" + Date.now() + path.extname(file.originalname);
+      cb(null, filename);
+    } catch (err) {
+      cb(err);
+    }
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only images are allowed!"), false);
+  }
+};
+
+// Initialize Multer with the storage configuration
+export const multerUploadHotelImages = multer({
+  storage: storage,
   fileFilter: fileFilter,
-})
+});
+
+export const multerUploadRoomImages = multer({
+  storage: storageForRoom,
+  fileFilter: fileFilter,
+});
