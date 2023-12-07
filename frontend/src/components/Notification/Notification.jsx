83cf363722dc0c15665ccac8559fb9e885ec480f
vinayakvenugopal <110168347+vinayakvenugopal@users.noreply.github.com>
2023-12-07 16:52:02 +0530

    notification ui

--- frontend/src/components/Notification/Notification.jsx
+++ frontend/src/components/Notification/Notification.jsx
@@ -1,24 +1,54 @@
-import React from "react";
-import { CDBNotification, CDBContainer } from "cdbreact";
-export const Notification = () => {
+import React, {useState} from 'react';
+import {Offcanvas} from 'react-bootstrap';
+
+const NotificationComponent = () => {
+  const [messages, setMessages] = useState([
+    {
+      senderName: 'John Doe',
+      content: 'This is a test notification.',
+      time: '2 hours ago',
+    },
+    {
+      senderName: 'Jane Doe',
+      content: 'This is another test notification.',
+      time: '1 hour ago',
+    },
+  ]);
+
+  const clearNotification = () => {
+    setMessages([]);
+  };
+
   return (
-    <CDBContainer className="position-relative" show >
-            <CDBNotification
-              show
-              fade
-              title="Contrast"
-              message="Hello, world! This is a positioned notification."
-              text="11 mins ago"
-              position="top-right"
-            />
-            <CDBNotification
-              show
-              fade
-              title="Contrast"
-              message="Hello, world! This is a positioned notification."
-              text="11 mins ago"
-              position="top-right"
-            />
-    </CDBContainer>
+    <Offcanvas show={true}>
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>Notifications</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <ul className="list-group">
+          {messages.map((message, index) => (
+            <li key={index} className="list-group-item">
+              <div className="d-flex">
+                <div className="flex-shrink-0 me-3">
+                  <svg width="16" height="16" fill="currentColor" className="rounded-circle" viewBox="0 0 16 16">
+                    <path d="M11.311 8.241c.004.991-.85.635-1.737.445L7.614 7.701a3.809 3.809 0 0 0-1.226.136c-1.324.21-2.458.627-3.159 1.242L1.07 11.26c.075-.279.227-.522.424-.744l4.595-4.595a3.605 3.605 0 0 1 1.223-.136c1.321-.211 2.45-.631 3.153-1.249l2.254-2.254a7.839 7.839 0 0 0 .423-.745L11.311 8.241z" />
+                  </svg>
+                </div>
+                <div className="flex-grow-1">
+                  <p className="mb-0">{message.senderName} messaged you:</p>
+                  <small className="text-muted">{message.content}</small>
+                </div>
+                <small className="text-muted">{message.time}</small>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <button type="button" className="btn btn-primary mt-3" onClick={clearNotification}>
+          Clear all
+        </button>
+      </Offcanvas.Body>
+    </Offcanvas>
   );
 };
+
+export default NotificationComponent;
--- frontend/src/components/UserNavbar/Header1.jsx
+++ frontend/src/components/UserNavbar/Header1.jsx
@@ -3,7 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { useLogoutMutation } from "../../slices/userApiSlice";
 import { logout } from "../../slices/userAuthSlice";
-import { Notification } from "../Notification/Notification";
+import Notification from "../Notification/Notification";
+
 const Header1 = () => {
   const navigate = useNavigate();
   const [navbar, setNavbar] = useState(false);
@@ -34,7 +35,6 @@ const Header1 = () => {
 
   return (
     <>
-    <Notification/>
       <header className={`header bg-dark-3 ${navbar ? "is-sticky" : ""}`}>
         <div className="header__container px-30 sm:px-20">
           <div className="row justify-between items-center">
@@ -116,6 +116,8 @@ const Header1 = () => {
                     </Link>
                   )}
                 </div>
+                <Notification />
+
 
                 <div className="d-none xl:d-flex x-gap-20 items-center pl-30 text-white">
                   <div>
