3f7385bc5f69101aaade8d9576f63527fe9db6d5
vinayakvenugopal <110168347+vinayakvenugopal@users.noreply.github.com>
2023-12-08 13:38:50 +0530

    correction in jwt user

--- api/middleware/userAuthMiddleware.js
+++ api/middleware/userAuthMiddleware.js
@@ -6,12 +6,15 @@ const protect = asyncHandler(async(req,res,next)=>{
     let token
     token = req.cookies.jwt
 
+
     if(token){
         try {
             const decoded = jwt.verify(token,process.env.JWT_SECRET)
+            console.log('decoded');
             req.user = await User.findById(decoded.userId).select('-password')
             next()
         } catch (error) {
+            console.log(error);
             res.status(401);
             throw new Error("Not Authorized ,invalid token")
         }
